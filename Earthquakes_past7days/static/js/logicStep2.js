5dde35bec1f1313988c717bd80572677922f8677
Hannah Stout <hanstout10@gmail.com>
2022-09-28 21:02:57 -0500

    logic step 2

---
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -0,0 +1,123 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>', 
+    maxZoom: 18,
+    // id: 'mapbox/streets-v11',
+    // tileSize: 512,
+    // zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    "Streets" : streets,
+    "Satellite" : satelliteStreets
+  };
+
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+
+// Then we add our 'graymap' tile layer to the map.
+//streets.addTo(map);
+
+
+// Accessing the airport GeoJSON URL
+//let airportData = "https://raw.githubusercontent.com/hanstout/Mapping_Earthquakes/main/majorAirports.json";
+
+
+// Accessing the Toronto airline routes GeoJSON URL.
+//let torontoData = "https://raw.githubusercontent.com/hanstout/Mapping_Earthquakes/main/torontoRoutes.json";
+
+
+// Accessing the Toronto neighborhoods GeoJSON URL.
+//let torontoHoods = "https://raw.githubusercontent.com/hanstout/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+
+// Create a style for the lines.
+//let myStyle = {
+//    color: "#ffffa1",
+ ///   weight: 2
+//}
+
+
+    // This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
+function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: "#ffae42",
+      color: "#000000",
+      radius: getRadius(),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+  // This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+  }
+
+
+
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data, {
+
+    // We turn each feature into a circleMarker on the map.
+    
+    pointToLayer: function(feature, latlng) {
+                console.log(data);
+                return L.circleMarker(latlng);
+            },
+          // We set the style for each circleMarker using our styleInfo function.
+        style: styleInfo
+        }).addTo(map);
+    
+
+
+    
+//L.geoJson(data, {
+      //  style: myStyle,
+      //  onEachFeature: function(feature, layer) {
+        //    layer.bindPopup("<h3> Airline: " + feature.properties.aireline + "</h3><hr><h3> Destination: "+ feature.properties.dst + "</h3>");
+
+//)
+   // })
+  // Creating a GeoJSON layer with the retrieved data.
+ // .addTo(map);
+//);
+
+
+// Different Map Styles that can be used in (id: '' ): 
+    // mapbox/streets-v11
+    // mapbox/outdoors-v11
+    // mapbox/light-v10
+    // mapbox/dark-v10
+    // mapbox/satellite-v9
+    // mapbox/satellite-streets-v11
+
